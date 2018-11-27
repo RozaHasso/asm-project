6c7737df75432ce3dd2452b5d2422a301cefa6d2
Roza Hasso <266235@via.dk>
2018-11-27 10:53:38 +0100

    combine the code

--- AssemblyProject/AssemblyProject/main.asm
+++ AssemblyProject/AssemblyProject/main.asm
@@ -1,9 +1,17 @@
 start:
 
 
-ldi r16, 5;;countdown time
+ldi r16, 0b0000_0000;countdown time
 out ddrb, r16; display time at port B
 
+
+
+
+;button codes
+ldi r26,0b0000_1000;stop countdown/beeping
+ldi r27, 0b0000_0001; increment
+ldi r28, 0b0000_0010; decrement
+ldi r29,0b0000_0100;start countdown
 
 ;initialization for reading input from buttons
 ldi		r20, 0x00
@@ -14,23 +22,48 @@ out     portc, r20
 
 
 
-;button codes
-ldi r26,0b0000_1000;stop countdown/beeping
-ldi r28,0b0000_0100;start countdown
+startloop:
 
-
-
- 
-
-startloop:; include inc/dec/start(loop)
-
-in r27, pinc  ;read from port c
-com r27
-cp r27,r28
-breq countdown
 
 out portb, r16
 
+in r25, pinc
+com r25
+cp r25, r27
+breq it_is
+	
+it_isnt:
+	in r25, pinc
+	com r25
+	cp r25, r28
+	breq it_is2 
+
+
+it_isnt2:
+
+in r25, pinc 
+com r25
+cp r25,r29
+breq countdown
+
+none:
+	jmp startloop
+
+it_is2:
+	dec r16
+	call delay
+	out ddrb, r16
+	out portb, r16
+	rjmp startloop
+	
+it_is:
+	inc r16
+	call delay
+	out ddrb, r16
+	out portb, r16
+	rjmp startloop
+
+ 
 countdown:
 
 	;check if cancel button pressed (should hold at least 1 sec)
@@ -101,66 +134,17 @@ delay_most_inner:
 	ret
 
 
-;Anatolie's part
-ldi r16, 0b0000_0000
-out ddrb, r16
-
-
-ldi r26, 0b0000_0001; increment
-ldi r27, 0b0000_0010; decrement
-
-;initialization for reading input from buttons
-ldi		r20, 0x00
-out		ddrc, r20
-ldi		r20, 0xff
-out     portc, r20
-
-start:
-out portb, r16
-
-in r25, pinc
-com r25
-cp r25, r26
-breq it_is
-	
-it_isnt:
-	in r25, pinc
-	com r25
-	cp r25, r27
-	breq it_is2 
-
-it_isnt2:
-	jmp start
-
-it_is2:
-	dec r16
-	call delay
-	out ddrb, r16
-	out portb, r16
-	rjmp start
-	
-it_is:
-	inc r16
-	call delay
-	out ddrb, r16
-	out portb, r16
-	rjmp start
 
 
 
-delay:
-	ldi r17, 0x65
-delay_outer:
-	ldi r18, 0xff
-delay_inner:
-	ldi r19, 0xff
-delay_most_inner:
-	dec r19
-	brne delay_most_inner
-	dec r18
-	brne delay_inner
-	dec r17
-	brne delay_outer
-	ret
+
+
+
+
+
+
+
+
+
 
 
